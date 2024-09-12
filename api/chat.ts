e90444f994fc11d912ffa8751aff68c69d17a9a8
Daniel Cunha <danielcc.jp@gmail.com>
2024-09-11 22:54:34 -0300

    chore: add env variable for deploy

--- api/chat.ts
+++ api/chat.ts
@@ -1,6 +1,6 @@
 import { Message } from "@/hooks/chat";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const getMessages = async (): Promise<Message[]> =>
   await fetch(`${API_URL}/`, {
